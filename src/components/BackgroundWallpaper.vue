a9f4afbe60bf40ea880f43810a0d6af9d78f3590
Hidden Goose <yanzhuangqiang@email.ncu.edu.cn>
2026-05-25 20:48:01 +0800

    fix: BackgroundWallpaper 改用静态 glob 导入修复生产环境壁纸 404

--- src/components/BackgroundWallpaper.vue
+++ src/components/BackgroundWallpaper.vue
@@ -1,32 +1,28 @@
-<!--
- * @Author: Hidden Goose yanzhuangqiang@email.ncu.edu.cn
- * @Date: 2026-04-27 15:23:45
- * @LastEditors: Hidden Goose yanzhuangqiang@email.ncu.edu.cn
- * @LastEditTime: 2026-04-27 17:21:49
- * @FilePath: \Hidden-goose_webBuild_project\src\components\BackgroundWallpaper.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
-  <div class="bg-wallpaper" :style="{ backgroundImage: `url(${imageSrc})` }"></div>
+  <div v-if="resolvedSrc" class="bg-wallpaper" :style="{ backgroundImage: `url(${resolvedSrc})` }"></div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+
 const props = defineProps({
-  imageSrc: {
-    type: String,
-    default: 'xiaguang.jpg'
-  }
+  imageSrc: { type: String, required: true }
 })
-const imageSrc = new URL(`../assets/${props.imageSrc}`, import.meta.url).href
+
+// 静态导入 wallpapers，确保构建时加上 hash
+const wallpapers = import.meta.glob('@/assets/*.{jpg,jpeg,png}', { eager: true, import: 'default' })
+
+const resolvedSrc = computed(() => {
+  const key = Object.keys(wallpapers).find(k => k.endsWith('/' + props.imageSrc))
+  return key ? wallpapers[key] : null
+})
 </script>
 
 <style scoped>
 .bg-wallpaper {
   position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  top: 0; left: 0;
+  width: 100%; height: 100%;
   z-index: 0;
   background-size: cover;
   background-position: center;
