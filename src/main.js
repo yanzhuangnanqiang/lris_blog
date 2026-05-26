19adab102fa7121b83e95d927f0a5f6644b44c3e
Hidden Goose <yanzhuangqiang@email.ncu.edu.cn>
2026-05-26 08:56:30 +0800

    feat: 添加 Vercel Analytics 统计

--- src/main.js
+++ src/main.js
@@ -10,7 +10,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
+import { inject } from '@vercel/analytics'
 import './style.css'
+
+inject()
 
 const app = createApp(App)
 app.use(createPinia())
